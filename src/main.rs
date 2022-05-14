53bd45fc812b53fc54666e4f27ce27216aca816e
Eddie Naff <eddie@eddienaff.dev>
2022-05-13 18:26:48 -0700

    Add a few primitive data types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+#[allow(unused_variables)] // So that there are no unused var warnings
+#[allow(unused_assignments)] // No unused assignment warnings, either
+
 fn main(){
-  println!("Hello world!");
+  // basic variable declaration
+  let some_data: bool = true;
+
+  // basic mutable variable declaration and assignment
+  let mut some_other_data: bool = true;
+  // reassignment
+  some_other_data = false;
+
+  // **INTEGERS**
+  // `i` below stands for integer
+  // `8` stands for "8 bits of memory"
+  // `i8` has enough memory to store 256 possible
+  // integers, including zero (2 to the 8th power) from
+  // -128 to +127
+  let some_integer: i8 = 16;
+
+  // To see these values...
+  println!("Max is: {}", std::i8::MAX);
+  println!("Min is: {}", std::i8::MIN)
 }
